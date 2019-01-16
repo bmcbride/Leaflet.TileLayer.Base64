4e399bb64143a05e1e0e8bf0f2513dcffaa345f8
Bryan McBride <mcbride.bryan@gmail.com>
2019-01-16 10:37:55 -0500

    set layer maxZoom to map maxZoom if not defined in layer options

--- demo/assets/vendor/Leaflet.TileLayer.Base64/Leaflet.TileLayer.Base64.js
+++ demo/assets/vendor/Leaflet.TileLayer.Base64/Leaflet.TileLayer.Base64.js
@@ -36,6 +36,7 @@ L.TileLayer.Base64 = L.TileLayer.extend({
     }
 
     this.options.maxNativeZoom = this.options.maxNativeZoom ? this.options.maxNativeZoom : maxNativeZoom;
+    this.options.maxZoom = this.options.maxZoom ? this.options.maxZoom : this._map.getMaxZoom();
 
     for (let i = 0; i < tiles.length; i++) {
       if (tiles[i].zoom_level == maxNativeZoom) {
